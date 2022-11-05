4afe9b892c7ab8b08b8cb4b5ae1f1746bbed3639
RichieWilli <richardwilliam1994@gmail.com>
2022-11-05 12:59:22 +0530

    New_Sample

---
+++ src/test/java/simplePackage/SeleniumScenariosTest.java
@@ -0,0 +1,24 @@
+package simplePackage;
+
+import org.testng.annotations.Test;
+
+public class SeleniumScenariosTest {
+	
+	@Test
+	public void sampleTest1() {
+		
+	System.out.println("hello Meet Yadav");
+	System.out.println("how are you");
+	
+	
+	}
+	@Test
+	public void sampleTest2() {
+		System.out.println("How is your work going");
+	}
+	
+	@Test
+	public void sampleTest3(){
+		System.out.println("where are you");
+	}
+}
